8bb66030a4e0b68a31c28889c2575accedc08232
Lucas Lima <lucaspecheche@gmail.com>
2019-07-14 06:07:23 -0300

    land

--- app/Telegram/Dialogs/Dialogs.php
+++ app/Telegram/Dialogs/Dialogs.php
@@ -2,9 +2,17 @@
 
 namespace App\Telegram\Dialogs;
 
+use Illuminate\Redis\RedisManager;
+use Telegram\Bot\Api;
+
 class Dialogs extends \BotDialogs\Dialogs
 {
     public $telegram;
+
+    public function __construct(Api $telegram, RedisManager $redis)
+    {
+        parent::__construct($telegram, $redis);
+    }
 
     public function schedule(string $chatId, int $step, $class)
     {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return response()->redirectTo('/land/index.html');
+
+});
 Route::prefix('telegram')->group(function () {
     Route::post('setWebhook', 'WebHookController@setWebhook');
     Route::post('removeWebhook', 'WebHookController@removeWebhook');
